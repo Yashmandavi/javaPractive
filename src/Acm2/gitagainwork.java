a10dbfe9a03b5041293569bd8549e9b64061100f
yashmanda <you@example.com>
2021-12-05 04:10:19 -0800

    New clas work

--- src/Acm2/gitagainwork.java
+++ src/Acm2/gitagainwork.java
@@ -1,2 +1,14 @@
-package Acm2;public class gitagainwork {
+package Acm2;
+
+public class gitagainwork {
+    /* creating branch in IJ to Git */
+
+    // git branch
+    // git checkout -b " name"
+    // git status
+    // git commit
+    // git add src
+
+
+
 }
